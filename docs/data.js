04b69094f3a3db29d33f06aefb17cbc161b03546
tobycassidy <76525529+tobycassidy@users.noreply.github.com>
2024-03-14 15:02:08 +0000

    docs with custom js

---
+++ docs/data.js
@@ -0,0 +1,158 @@
+var json_data = {"name": "aml", "children": [{"name": "activitybasedsanction","description":"test1", "children": [{"name": "commercial", "children": [{"name": "sanctions"}]}, {"name": "general", "children": [{"name": "sanctions"}]}, {"name": "immigration", "children": [{"name": "sanction"}]}, {"name": "other", "children": [{"name": "sanctions"}]}, {"name": "shippingaircraft", "children": [{"name": "sanction"}]}, {"name": "supplyofgoods", "children": [{"name": "export"}, {"name": "import"}]}, {"name": "supplyofservices", "children": [{"name": "export"}, {"name": "import"}]}, {"name": "technologytransfer", "children": [{"name": "restriction"}]}]}, {"name": "assetfreeze", "children": [{"name": "access", "children": [{"name": "frozenfunds"}]}, {"name": "competentauthority", "children": [{"name": "vigilance"}]}, {"name": "courtreview", "children": [{"name": "sanctions"}]}, {"name": "fortfeiture", "children": [{"name": "funds"}]}, {"name": "information", "children": [{"name": "financialinstitutions"}]}, {"name": "injunctions", "children": [{"name": "req"}]}, {"name": "investigation", "children": [{"name": "procedure"}]}, {"name": "procedure", "children": [{"name": "req"}]}, {"name": "supervisoryauthority", "children": [{"name": "obligations"}]}]}, {"name": "bo", "children": [{"name": "information", "children": [{"name": "requirements"}]}, {"name": "legalarrangements", "children": [{"name": "requirements"}]}, {"name": "legalentities", "children": [{"name": "requirements"}]}, {"name": "register", "children": [{"name": "accessinfo"}, {"name": "activeaccounts"}]}]}, {"name": "cdd", "children": [{"name": "additional", "children": [{"name": "cashcouriers"}, {"name": "funds"}, {"name": "insurance"}, {"name": "msb"}]}, {"name": "correspondentbank", "children": [{"name": "considerations"}, {"name": "geographicrisk"}, {"name": "obligation"}]}, {"name": "counterparty", "children": [{"name": "anonymous"}, {"name": "pep"}]}, {"name": "enhanced", "children": [{"name": "anonymity"}, {"name": "apparent"}, {"name": "higherrisk"}, {"name": "obligation"}]}, {"name": "exemption", "children": [{"name": "clients"}]}, {"name": "measures", "children": [{"name": "extent"}, {"name": "purposerelationship"}, {"name": "thirdparty"}]}, {"name": "mis", "children": [{"name": "obligation"}]}, {"name": "monitoring", "children": [{"name": "ongoingdiligence"}, {"name": "type"}]}, {"name": "obligation", "children": [{"name": "triggerevent"}]}, {"name": "prohibition", "children": [{"name": "business"}, {"name": "refuseservices"}, {"name": "shellbanks"}]}, {"name": "rba", "children": [{"name": "measures"}]}, {"name": "simplified", "children": [{"name": "lowrisk"}]}]}, {"name": "competentauthority", "children": [{"name": "collaboration", "children": [{"name": "domestic"}]}, {"name": "dnfpb", "children": [{"name": "regulation"}]}, {"name": "lawenforcement", "children": [{"name": "cooperation"}, {"name": "information"}]}, {"name": "regulation", "children": [{"name": "identity"}, {"name": "licensing"}, {"name": "sroduties"}, {"name": "sroidentity"}]}]}, {"name": "customeridentification", "children": [{"name": "exemption", "children": [{"name": "kyc"}]}, {"name": "identificationdocs", "children": [{"name": "acceptable"}]}, {"name": "kyc", "children": [{"name": "corporates"}, {"name": "individuals"}]}, {"name": "remoteonboarding", "children": [{"name": "nonfacetoface"}]}, {"name": "restriction", "children": [{"name": "accountopening"}]}, {"name": "tieredkyc", "children": [{"name": "highvalue"}, {"name": "lowvalue"}, {"name": "mediumvalue"}]}, {"name": "update", "children": [{"name": "kyc "}, {"name": "kycnoncustomer"}]}, {"name": "verification", "children": [{"name": "agents"}, {"name": "beneficialowners"}, {"name": "existingcustomers"}, {"name": "individuals"}, {"name": "partnership"}, {"name": "rba"}, {"name": "timing"}, {"name": "trustees"}]}]}, {"name": "definitions", "children": [{"name": "applicablefirms", "children": [{"name": "reportingentity"}]}, {"name": "asset", "children": [{"name": "sanction"}]}, {"name": "beneficialownership", "children": [{"name": "significantcontrol"}]}, {"name": "freeze", "children": [{"name": "sanction"}]}, {"name": "funds", "children": [{"name": "sanction"}]}, {"name": "listing", "children": [{"name": "meaning"}]}, {"name": "moneylaundering", "children": [{"name": "meaning"}]}, {"name": "pep", "children": [{"name": "meaning"}]}, {"name": "sanction", "children": [{"name": "meaning"}]}, {"name": "terroristfinancing", "children": [{"name": "meaning"}]}, {"name": "tfs", "children": [{"name": "sanctioncountry"}, {"name": "sanctionthematic"}]}, {"name": "withoutdelay", "children": [{"name": "sanction"}]}]}, {"name": "intlcooperation", "children": [{"name": "extradition", "children": [{"name": "arrest"}]}, {"name": "information", "children": [{"name": "sharing"}]}, {"name": "legalassistance", "children": [{"name": "information"}]}, {"name": "tfs", "children": [{"name": "international"}]}]}, {"name": "mlro", "children": [{"name": "appointment", "children": [{"name": "obligation"}]}, {"name": "fitproper", "children": [{"name": "test"}]}, {"name": "internalreporting", "children": [{"name": "obligation"}]}, {"name": "obligations", "children": [{"name": "powers"}]}, {"name": "reporting", "children": [{"name": "obligation"}]}, {"name": "resourcing", "children": [{"name": "requirements"}]}]}, {"name": "pcp", "children": [{"name": "agent", "children": [{"name": "fitproper"}, {"name": "riskexposure"}, {"name": "training"}]}, {"name": "amlprogram", "children": [{"name": "interprocedures"}]}, {"name": "audit", "children": [{"name": "function"}]}, {"name": "complianceofficer", "children": [{"name": "appointment"}]}, {"name": "employees", "children": [{"name": "screeningprocedures"}]}, {"name": "grouplevel", "children": [{"name": "additional"}, {"name": "amlmeasures"}, {"name": "equivalent"}]}, {"name": "training", "children": [{"name": "employees"}]}]}, {"name": "penalties", "children": [{"name": "administrativepenalties", "children": [{"name": "offence"}]}, {"name": "cdd", "children": [{"name": "offence"}]}, {"name": "civilpenalties", "children": [{"name": "offence"}]}, {"name": "corporateliability", "children": [{"name": "offence"}]}, {"name": "criminalpenalties", "children": [{"name": "offence"}]}, {"name": "moneylaundering", "children": [{"name": "offence"}]}, {"name": "recordkeeping", "children": [{"name": "offence"}]}, {"name": "reporting", "children": [{"name": "offence"}]}, {"name": "sanctions", "children": [{"name": "offence"}]}, {"name": "suspiciousreporting", "children": [{"name": "offence"}]}, {"name": "terroristfinancing", "children": [{"name": "offence"}]}]}, {"name": "recordkeeping", "children": [{"name": "cdd", "children": [{"name": "obligation"}]}, {"name": "competentauthorities", "children": [{"name": "obligation"}]}, {"name": "format", "children": [{"name": "contents"}]}, {"name": "pcp", "children": [{"name": "gifts"}, {"name": "obligation"}]}, {"name": "thirdparties", "children": [{"name": "reliance"}]}, {"name": "transactions", "children": [{"name": "activeaccounts"}, {"name": "closedaccounts"}]}]}, {"name": "riskassessment", "children": [{"name": "correspondentbanking", "children": [{"name": "frequency"}, {"name": "obligation"}, {"name": "ongoing"}]}, {"name": "customerrisk", "children": [{"name": "riskprofiling"}]}, {"name": "firm", "children": [{"name": "exposure"}]}, {"name": "highlevel", "children": [{"name": "criteria"}, {"name": "proportionality"}]}, {"name": "newtechnology", "children": [{"name": "riskmitigation"}]}, {"name": "npo", "children": [{"name": "rba"}]}, {"name": "periodicrba", "children": [{"name": "jurisdiction"}, {"name": "supervisoryauthorities"}]}]}, {"name": "sanctionrights", "children": [{"name": "compensation", "children": [{"name": "grievance"}]}, {"name": "grievance", "children": [{"name": "redressal"}]}, {"name": "listedentity", "children": [{"name": "rights"}]}]}, {"name": "str", "children": [{"name": "exemption", "children": [{"name": "reporting"}]}, {"name": "firms", "children": [{"name": "definition"}, {"name": "format"}, {"name": "investigation"}, {"name": "reporting"}, {"name": "thresholdtransaction"}, {"name": "tippingoff"}]}, {"name": "fiu", "children": [{"name": "governance"}, {"name": "identity"}, {"name": "obligation"}, {"name": "powers"}, {"name": "reporting"}]}, {"name": "immunity", "children": [{"name": "prosecutions"}]}, {"name": "sanctions", "children": [{"name": "breaches"}]}, {"name": "supervisory", "children": [{"name": "suspicioustransactions"}]}]}, {"name": "tfs", "children": [{"name": "dealingassets", "children": [{"name": "dealing"}, {"name": "fundavailability"}]}, {"name": "delisting", "children": [{"name": "criteria"}]}, {"name": "designation", "children": [{"name": "sanction"}]}, {"name": "duration", "children": [{"name": "order"}]}, {"name": "exceptions", "children": [{"name": "sanctions"}]}, {"name": "financialinstitutions", "children": [{"name": "obligation"}]}, {"name": "freezeorder", "children": [{"name": "particulars"}]}, {"name": "grounds", "children": [{"name": "listing"}]}, {"name": "intlsanctions", "children": [{"name": "foreignlist"}, {"name": "locallist"}]}, {"name": "liftingsanctions", "children": [{"name": "criteria"}]}, {"name": "publication", "children": [{"name": "sanctionlist"}]}, {"name": "reporting", "children": [{"name": "assetfreeze"}]}, {"name": "sanctionspermit", "children": [{"name": "authorisation"}]}]}, {"name": "wiretransfer", "children": [{"name": "information", "children": [{"name": "beneficiary"}, {"name": "originator"}]}, {"name": "monitoring", "children": [{"name": "reporting"}]}]}]};
+var m = [20, 120, 20, 60],
+    w = 1920 - m[1] - m[3],
+    h = 900 - m[0] - m[2],
+    i = 0,
+    root;
+
+var tree = d3.layout.tree()
+    .size([h, w]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+var vis = d3.select("#body").append("svg:svg")
+    .attr("width", w + m[1] + m[3])
+    .attr("height", h + m[0] + m[2])
+  .append("svg:g")
+    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+root = json_data;
+root.x0 = h / 2;
+root.y0 = 0;
+
+function toggleAll(d) {
+  if (d.children) {
+    d.children.forEach(toggleAll);
+    toggle(d);
+  }
+}
+
+root.children.forEach(toggleAll);
+
+
+update(root);
+
+
+function update(source) {
+  var duration = d3.event && d3.event.altKey ? 5000 : 500;
+
+  // Compute the new tree layout.
+  var nodes = tree.nodes(root).reverse();
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d) { d.y = d.depth * 350; });
+
+  // Update the nodes…
+  var node = vis.selectAll("g.node")
+      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+  // Enter any new nodes at the parent's previous position.
+  var nodeEnter = node.enter().append("svg:g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+      .on("click", function(d) { toggle(d); update(d); });
+
+  nodeEnter.append("svg:circle")
+      .attr("r", 1e-6)
+      .style("fill", function(d) { return d._children ? "#F2B847" : "#FFFFFF"; });
+
+  nodeEnter.append('a')
+      .attr('xlink:href', function(d) {
+          return d.url;
+      })
+      .append("svg:text")
+      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+      .attr("dy", ".35em")
+      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+      .text(function(d) { return d.name; })
+      .style('fill', function(d) {
+          return d.free ? 'black' : '#FFFFFF';
+      })
+      .style('font-weight', 500)
+      .style("fill-opacity", 1e-6);
+
+  nodeEnter.append("svg:title")
+    .text(function(d) {
+      return d.description;
+    });
+
+  // Transition nodes to their new position.
+  var nodeUpdate = node.transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+  nodeUpdate.select("circle")
+      .attr("r", 6)
+      .style("fill", function(d) { return d._children ? "#F2B847" : "#FFFFFF"; });
+
+  nodeUpdate.select("text")
+      .style("fill-opacity", 1);
+
+  // Transition exiting nodes to the parent's new position.
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+      .remove();
+
+  nodeExit.select("circle")
+      .attr("r", 1e-6);
+
+  nodeExit.select("text")
+      .style("fill-opacity", 1);
+
+  // Update the links…
+  var link = vis.selectAll("path.link")
+      .data(tree.links(nodes), function(d) { return d.target.id; });
+
+  // Enter any new links at the parent's previous position.
+  link.enter().insert("svg:path", "g")
+      .attr("class", "link")
+      .attr("d", function(d) {
+        var o = {x: source.x0, y: source.y0};
+        return diagonal({source: o, target: o});
+      })
+    .transition()
+      .duration(duration)
+      .attr("d", diagonal);
+
+  // Transition links to their new position.
+  link.transition()
+      .duration(duration)
+      .attr("d", diagonal);
+
+  // Transition exiting nodes to the parent's new position.
+  link.exit().transition()
+      .duration(duration)
+      .attr("d", function(d) {
+        var o = {x: source.x, y: source.y};
+        return diagonal({source: o, target: o});
+      })
+      .remove();
+
+  // Stash the old positions for transition.
+  nodes.forEach(function(d) {
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+}
+
+// Toggle children.
+function toggle(d) {
+  if (d.children) {
+    d._children = d.children;
+    d.children = null;
+  } else {
+    d.children = d._children;
+    d._children = null;
+  }
+}
+
+// var json_data = {
+//     "name": " ",
+//     "children": [
+//       {
+//        "name": "Interactive tools",
+//        "free": true,
+//        "description": "Interactive authoring tools",
+//        "children": [
